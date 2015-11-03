e34a82e4b53dfdd698277093c3d79cc9d2f556e1
fantastickford1 <carlitos_zenteno@hotmail.com>
2015-11-03 00:30:04 -0600

    Micro Paint V. 3 :facepunch:
    -Everything is fixed-

--- MicroPaintV1/src/micropaint/FXMLDocumentController.java
+++ MicroPaintV1/src/micropaint/FXMLDocumentController.java
@@ -5,7 +5,6 @@
  */
 package micropaint;
 
-import com.sun.javafx.sg.prism.NGCanvas;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -15,125 +14,194 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 
 /**
  *
- * @author AngelRuiz
+ * @author CarlosA.
  */
 public class FXMLDocumentController implements Initializable {
-    
-    private GraphicsContext[] gcs;
-    private GraphicsContext gc;
-    private Color color;
-    private int fig=0;
+    //>>>>>>>>>>>>>>>>>>>>>>>Other variables<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    private GraphicsContext gcB,gcF;
     private boolean drawline = false,drawoval = false,drawrectangle = false,erase = false,freedesign = true;
-    double startX = -1, startY = -1, lastX,lastY;
+    double startX, startY, lastX,lastY,oldX,oldY;
     double hg;
- 
+    //>>>>>>>>>>>>>>>>>>>>>>>FXML Variables<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @FXML private RadioButton strokeRB,fillRB;
-    @FXML private Slider sliderSize;
     @FXML private ColorPicker colorPick;
-    @FXML private Canvas TheCanvas;
+    @FXML private Canvas TheCanvas,canvasGo;
     @FXML private Button rectButton,lineButton,ovlButton,pencButton;
-    @FXML private MenuItem eraser;
+    @FXML private Slider sizeSlider;
+    //////////////////////////////////////////////////////////////////////////////
     
-    
-    @FXML 
-    private void clearCanvas(ActionEvent e){
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, 515, 453);
-    }
-    
-    //private int bufferFig=0;
-    
+
     @FXML
     private void onMousePressedListener(MouseEvent e){
-        
-        sliderSize.setMin(0);
-        sliderSize.setMax(300);
-        this.hg = sliderSize.getValue();
-        
-        gcs[fig]=TheCanvas.getGraphicsContext2D();
-        
         this.startX = e.getX();
         this.startY = e.getY();
-        System.err.println("mousePressed at" + startX + ", "+ startY);
-        if(drawoval){  
-            this.dibujaOValo();
-        }
-        if(drawrectangle)
-            this.dibujaRect();
-    }
-    
-    @FXML
-    private void onMouseReleaseListener(MouseEvent e){
-        this.lastX = e.getX();
-        this.lastY = e.getY();
-        if(drawline)
-            this.dibujarLinea();
-        fig++;
+        this.oldX = e.getX();
+        this.oldY = e.getY();
     }
     
     @FXML
     private void onMouseDraggedListener(MouseEvent e){
-        if(erase){
-            gc.setFill(Color.WHITE);
-            gc.setStroke(Color.WHITE);
-            gc.fillOval(e.getX(), e.getY(), hg, hg);
-        }
-    }
-
-    private void dibujaOValo(){
-        gcs[fig].setFill(colorPick.getValue());
-        gcs[fig].setStroke(colorPick.getValue());
-        if(strokeRB.isSelected() == true){
-            gcs[fig].strokeOval(startX, startY, hg, hg);
-        }else
-            gcs[fig].fillOval(startX, startY, hg, hg);
-    }
-    
-    private void dibujaRect(){
-        gcs[fig].setStroke(colorPick.getValue());
-        gcs[fig].setFill(colorPick.getValue());
-        if(strokeRB.isSelected() == true){
-            gcs[fig].strokeRect(startX, startY, hg, hg);
-        }else
-            gcs[fig].fillRect(startX, startY, hg, hg);
-     
-    }
-    
-    private void dibujarLinea(){
-        gcs[fig].setFill(colorPick.getValue());
-        gcs[fig].setStroke(colorPick.getValue());
-        gcs[fig].setLineWidth(5);
-        gcs[fig].strokeLine(startX, startY, lastX, lastY);
-    }
-    
-   @FXML
-    private void onMouseExitedListener(MouseEvent event){
-        System.out.println("No puedes dibujar fuera del canvas");
+        this.lastX = e.getX();
+        this.lastY = e.getY();
+        
+        if(drawrectangle)
+            drawRectEffect();
+        if(drawoval)
+            drawOvalEffect();
+        if(drawline)
+            drawLineEffect();
+        if(freedesign)
+            freeDrawing();
+        if(erase)
+            eraser();
     }
     
     @FXML
-    private void setOvalAsCurrentShape(ActionEvent e){
+    private void onMouseReleaseListener(MouseEvent e){
+        if(drawrectangle)
+            drawRect();
+        if(drawoval)
+            drawOval();
+        if(drawline)
+            drawLine();
+    }
+    
+    @FXML
+    private void onMouseExitedListener(MouseEvent event)
+    {
+        System.out.println("No puedes dibujar fuera del canvas");
+    }
+    
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>Draw methods<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    
+    private void drawOval()
+    {
+        double wh = lastX - startX;
+        double hg = lastY - startY;
+        gcB.setLineWidth(sizeSlider.getValue());
+        
+        if(fillRB.isSelected()){
+            gcB.setFill(colorPick.getValue());
+            gcB.fillOval(startX, startY, wh, hg);
+        }else{
+            gcB.setStroke(colorPick.getValue());
+            gcB.strokeOval(startX, startY, wh, hg);
+        }
+    }
+    
+    private void drawRect()
+    {
+        double wh = lastX - startX;
+        double hg = lastY - startY;
+        gcB.setLineWidth(sizeSlider.getValue());
+        
+        if(fillRB.isSelected()){
+            gcB.setFill(colorPick.getValue());
+            gcB.fillRect(startX, startY, wh, hg);
+        }else{
+            gcB.setStroke(colorPick.getValue());
+            gcB.strokeRect(startX, startY, wh, hg);
+        }
+    }
+ 
+    private void drawLine()
+    {
+        gcB.setLineWidth(sizeSlider.getValue());
+        gcB.setStroke(colorPick.getValue());
+        gcB.strokeLine(startX, startY, lastX, lastY);
+    }
+    
+    private void freeDrawing()
+    {
+        gcB.setLineWidth(sizeSlider.getValue());
+        gcB.setStroke(colorPick.getValue());
+        gcB.strokeLine(oldX, oldY, lastX, lastY);
+        oldX = lastX;
+        oldY = lastY;
+    }
+    
+    private void eraser()
+    {
+        gcB.clearRect(lastX, lastY, sizeSlider.getValue(), sizeSlider.getValue());
+        gcF.clearRect(lastX, lastY, sizeSlider.getValue(), sizeSlider.getValue());
+    }
+    
+    //////////////////////////////////////////////////////////////////////
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>Draw effects methods<<<<<<<<<<<<<<<<<<<<<<<
+    
+    private void drawOvalEffect()
+    {
+        double wh = lastX - startX;
+        double hg = lastY - startY;
+        gcF.setLineWidth(sizeSlider.getValue());
+        
+        if(fillRB.isSelected()){
+            gcF.clearRect(0, 0, canvasGo.getWidth(), canvasGo.getHeight());
+            gcF.setFill(colorPick.getValue());
+            gcF.fillOval(startX, startY, wh, hg);
+        }else{
+            gcF.clearRect(0, 0, canvasGo.getWidth(), canvasGo.getHeight());
+            gcF.setStroke(colorPick.getValue());
+            gcF.strokeOval(startX, startY, wh, hg );
+        }
+       }
+    
+    private void drawRectEffect()
+    {
+        double wh = lastX - startX;
+        double hg = lastY - startY;
+        gcF.setLineWidth(sizeSlider.getValue());
+        
+        if(fillRB.isSelected()){
+            gcF.clearRect(0, 0, canvasGo.getWidth(), canvasGo.getHeight());
+            gcF.setFill(colorPick.getValue());
+            gcF.fillRect(startX, startY, wh, hg);
+        }else{
+            gcF.clearRect(0, 0, canvasGo.getWidth(), canvasGo.getHeight());
+            gcF.setStroke(colorPick.getValue());
+            gcF.strokeRect(startX, startY, wh, hg );
+        }
+    }
+    
+    private void drawLineEffect()
+    {
+        gcF.setLineWidth(sizeSlider.getValue());
+        gcF.setStroke(colorPick.getValue());
+        gcF.clearRect(0, 0, canvasGo.getWidth() , canvasGo.getHeight());
+        gcF.strokeLine(startX, startY, lastX, lastY);
+    }
+    ///////////////////////////////////////////////////////////////////////
+    
+    @FXML 
+    private void clearCanvas(ActionEvent e)
+    {
+        gcB.clearRect(0, 0, TheCanvas.getWidth(), TheCanvas.getHeight());
+        gcF.clearRect(0, 0, TheCanvas.getWidth(), TheCanvas.getHeight());
+    }
+        
+    
+    //>>>>>>>>>>>>>>>>>>>>>Buttons control<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @FXML
+    private void setOvalAsCurrentShape(ActionEvent e)
+    {
         drawline = false;
         drawoval = true;
         drawrectangle = false;
         freedesign = false;
         erase = false;
-        
     }
     
      @FXML
-    private void setLineAsCurrentShape(ActionEvent e){
+    private void setLineAsCurrentShape(ActionEvent e)
+    {
         drawline = true;
         drawoval = false;
         drawrectangle = false;
@@ -141,7 +209,8 @@ public class FXMLDocumentController implements Initializable {
         erase = false;
     }
      @FXML
-    private void setRectangleAsCurrentShape(ActionEvent e){
+    private void setRectangleAsCurrentShape(ActionEvent e)
+    {
         drawline = false;
         drawoval = false;
         freedesign = false;
@@ -150,7 +219,8 @@ public class FXMLDocumentController implements Initializable {
     }
     
     @FXML
-    private void setErase(ActionEvent e){
+    private void setErase(ActionEvent e)
+    {
         drawline = false;
         drawoval = false;
         drawrectangle = false;    
@@ -159,7 +229,8 @@ public class FXMLDocumentController implements Initializable {
     }
     
     @FXML
-    private void setFreeDesign(ActionEvent e){
+    private void setFreeDesign(ActionEvent e)
+    {
         drawline = false;
         drawoval = false;
         drawrectangle = false;    
@@ -167,14 +238,17 @@ public class FXMLDocumentController implements Initializable {
         freedesign = true;
     }
     
+    //////////////////////////////////////////////////////////////////
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        gcs = new GraphicsContext[100];
-        gc=TheCanvas.getGraphicsContext2D();        
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, 515, 453);
+        gcB = TheCanvas.getGraphicsContext2D();
+        gcF = canvasGo.getGraphicsContext2D();
+        
+        sizeSlider.setMin(1);
+        sizeSlider.setMax(50);
+        
         //////////////////////////////////
         Image imageRect = new Image(getClass().getResourceAsStream("Stop-32.png"));
         ImageView icR = new ImageView(imageRect);
--- MicroPaintV1/src/micropaint/MicroPaint.java
+++ MicroPaintV1/src/micropaint/MicroPaint.java
@@ -11,10 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author AngelRuiz
- */
+
 public class MicroPaint extends Application {
     
     @Override
